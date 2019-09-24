eb43caa01c7c6b1caff6f018d456e55ed655c705
Luke Barnes <lbarnes@digitalriver.com>
2019-09-24 13:51:55 -0500

    Commit

--- dist/components/cc-number/cc-number.js
+++ dist/components/cc-number/cc-number.js
@@ -19544,20 +19544,18 @@ function getComponentData() {
  */
 
 function handleChange(event) {
-  console.log('component data', getComponentData());
-
   if (typeof getComponentData() === 'undefined') {
-    console.log('component data not provided!');
+    console.error('component data not provided!');
     return;
   } // FIXME Works differently if I add a value and tab out.
   // TODO Could add a state variable that ignore if no focus has been recieved yet...
 
 
-  console.log('6', event);
-  console.log('cc number on change 1 .', event.target.value);
+  console.log('7', event);
+  console.log('cc number on change 1', event.target.value);
   var initialEventValue = event.target.value;
   event.target.value = Object(_utils_js__WEBPACK_IMPORTED_MODULE_6__["stripLetters"])(initialEventValue);
-  console.log('cc number on change 2 ', event.target.value);
+  console.log('cc number on change 2', event.target.value);
 
   if (Object(_utils_js__WEBPACK_IMPORTED_MODULE_6__["checkToSendEvent"])(initialEventValue, event)) {
     console.log('cc number on change checkToSendEvent', event.target.value);
