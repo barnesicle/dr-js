01858ebf9f0391063a1ab09325d2e8006c1ee4e3
Luke Barnes <lbarnes@digitalriver.com>
2019-09-24 08:49:10 -0500

    Commit

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 # Requires cd /c/dev/ui-architecture/drjs-demo 
-cp -r ../digitalriverpayments/dist/ ./ && git add . && git commit -m "Commit" --amend && git push lbarnes master --force
+cp -r ../digitalriverpayments/dist/ ./ && git add . && git commit -m "Commit" && git push lbarnes master
