79b1635f1b16653e0e54af8be33e873449aec080
Luke Barnes <lbarnes@digitalriver.com>
2019-09-24 12:25:03 -0500

    Commit

--- dist/components/cc-number/cc-number.js
+++ dist/components/cc-number/cc-number.js
@@ -19545,16 +19545,16 @@ function getComponentData() {
 
 function handleChange(event) {
   // FIXME Works differently if I add a value and tab out.
-  console.log('cc number on change 1...', event.target.value);
+  console.log('cc number on change 1....', event.target.value);
   var initialEventValue = event.target.value;
   event.target.value = Object(_utils_js__WEBPACK_IMPORTED_MODULE_6__["stripLetters"])(initialEventValue);
-  console.log('cc number on change 2...', event.target.value);
-  /*if (checkToSendEvent(initialEventValue, event)) {
-     console.log('cc number on change checkToSendEvent', event.target.value)
-    //handleEvent(componentData, 'change', event);
-     //formatNumber(event);
-     //setMaxLengthFromCurrentValueLength();
-  }*/
+  console.log('cc number on change 2....', event.target.value);
+
+  if (Object(_utils_js__WEBPACK_IMPORTED_MODULE_6__["checkToSendEvent"])(initialEventValue, event)) {
+    console.log('cc number on change checkToSendEvent', event.target.value); //handleEvent(componentData, 'change', event);
+    //formatNumber(event);
+    //setMaxLengthFromCurrentValueLength();
+  }
 }
 /**
  * handleAutofill accepts an autofill event and sends it on to handleEvent
