6047f01c91c79d8d726997b17e679e1413e6a768
Luke Barnes <lbarnes@digitalriver.com>
2020-03-09 12:29:36 -0500

    Commit

--- DigitalRiver.js
+++ DigitalRiver.js
@@ -24156,7 +24156,7 @@ function addStandaloneButtonOptions(component, options) {
         amount: 10,
         detail: 'Will arrive tomorrow morning'
       }],
-      total: '101.00',
+      total: 101.00,
       style: {
         buttonType: 'buy',
         buttonColor: 'light',
@@ -24220,7 +24220,7 @@ function mountDropin(key, options, createSource, createElement) {
         },
         paymentMethods: paymentMethodResponse
       };
-      console.error('5');
+      console.error('6');
       mockedResponse.paymentMethods.forEach(function (availablePaymentMethod) {
         var paymentMethod = supportedPaymentMethods.find(function (paymentMethod) {
           return paymentMethod.type === availablePaymentMethod.type;
