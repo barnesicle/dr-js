248b6e6e7168d614554ed34d4e807b7500434d89
Luke Barnes <lukedbarnes@gmail.com>
2020-04-27 12:47:44 -0500

    Commit

--- components/controller/controller.js
+++ components/controller/controller.js
@@ -20981,7 +20981,7 @@ function sanitizeInputForPaymentApi(input) {
 /*!*************************************!*\
   !*** ./src/app/components/utils.js ***!
   \*************************************/
-/*! exports provided: isShallowEquivalent, stripLetters, stripLettersAndAddForwardSlash, getParentDomain, checkToSendEvent, spacesAdded, removeExtraCharacters, isIEBrowser */
+/*! exports provided: isShallowEquivalent, stripLetters, stripLettersAndAddForwardSlash, getParentDomain, checkToSendEvent, spacesAdded, removeExtraCharacters */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -20993,7 +20993,6 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "checkToSendEvent", function() { return checkToSendEvent; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "spacesAdded", function() { return spacesAdded; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "removeExtraCharacters", function() { return removeExtraCharacters; });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "isIEBrowser", function() { return isIEBrowser; });
 /* harmony import */ var cross_domain_utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! cross-domain-utils */ "./node_modules/cross-domain-utils/dist/module/index.js");
 
 /**
@@ -21090,47 +21089,6 @@ function removeExtraCharacters(value, maxLength) {
   }
 
   return value;
-}
-/**
- * detect IE
- * returns version of IE or false, if browser is not Internet Explorer
- */
-
-function isIEBrowser() {
-  var ua = window.navigator.userAgent; // Test values; Uncomment to check result …
-  // IE 10
-  // ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
-  // IE 11
-  // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
-  // Edge 12 (Spartan)
-  // ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
-  // Edge 13
-  // ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
-
-  var msie = ua.indexOf('MSIE ');
-
-  if (msie > 0) {
-    // IE 10 or older => return version number
-    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-  }
-
-  var trident = ua.indexOf('Trident/');
-
-  if (trident > 0) {
-    // IE 11 => return version number
-    var rv = ua.indexOf('rv:');
-    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-  }
-
-  var edge = ua.indexOf('Edge/');
-
-  if (edge > 0) {
-    // Edge (IE 12+) => return version number
-    return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-  } // other browser
-
-
-  return false;
 }
 
 /***/ }),
